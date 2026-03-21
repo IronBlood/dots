8608e790a38aa734a35a06805033dc27f64c8433
IronBlood <aplshenyang@gmail.com>
2026-03-21 22:12:08 +0800

    treat untracked file dirty

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,6 +2,7 @@ function fish_prompt
 	set -g fish_prompt_pwd_dir_length 0
 
 	set -g __fish_git_prompt_showdirtystate 1
+	set -g __fish_git_prompt_showuntrackedfiles 1
 	set -g __fish_git_prompt_showupstream none
 	set -g __fish_git_prompt_showstashstate 0
 	set -g __fish_git_prompt_showuntrackedfiles 0
