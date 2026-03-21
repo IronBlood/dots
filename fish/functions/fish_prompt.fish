1f5ed3aec8d56d9d6e17ce8ce7be88f7979c01df
IronBlood <aplshenyang@gmail.com>
2026-03-21 22:03:06 +0800

    show full path

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt
+	set -g fish_prompt_pwd_dir_length 0
+
 	set -g __fish_git_prompt_showdirtystate 1
 	set -g __fish_git_prompt_showupstream none
 	set -g __fish_git_prompt_showstashstate 0
