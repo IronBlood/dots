2468fedf4592a4e63b115a6776a6e75d65f0d0e6
IronBlood <aplshenyang@gmail.com>
2026-03-21 22:14:59 +0800

    refactor: move settings to config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
 if status is-interactive
+	set -g fish_prompt_pwd_dir_length 0
+
+	set -g __fish_git_prompt_showdirtystate 1
+	set -g __fish_git_prompt_showuntrackedfiles 1
+	set -g __fish_git_prompt_showupstream none
+	set -g __fish_git_prompt_showstashstate 0
+	set -g __fish_git_prompt_show_informative_status 0
+
+	set -g __fish_git_prompt_color_branch cyan
+	set -g __fish_git_prompt_color_dirtystate red
+	set -g __fish_git_prompt_color_cleanstate green
+	set -g __fish_git_prompt_char_dirtystate '✖︎'
+	set -g __fish_git_prompt_char_cleanstate ' ●'
+
 	fish_config theme choose catppuccin-mocha --color-theme=dark
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,19 +1,4 @@
 function fish_prompt
-	set -g fish_prompt_pwd_dir_length 0
-
-	set -g __fish_git_prompt_showdirtystate 1
-	set -g __fish_git_prompt_showuntrackedfiles 1
-	set -g __fish_git_prompt_showupstream none
-	set -g __fish_git_prompt_showstashstate 0
-	set -g __fish_git_prompt_showuntrackedfiles 0
-	set -g __fish_git_prompt_show_informative_status 0
-
-	set -g __fish_git_prompt_color_branch cyan
-	set -g __fish_git_prompt_color_dirtystate red
-	set -g __fish_git_prompt_color_cleanstate green
-	set -g __fish_git_prompt_char_dirtystate '✖︎'
-	set -g __fish_git_prompt_char_cleanstate ' ●'
-
 	set -l user_color   cyan
 	set -l host_color   green
 	set -l dir_color    yellow
