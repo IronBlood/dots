8ae78d61031bd93ca23778626bfa13ce555d1ef7
IronBlood <aplshenyang@gmail.com>
2026-06-14 11:47:05 +0800

    map uv cache and use copy mode instead of hardlink

--- fish/functions/uv-docker.fish
+++ fish/functions/uv-docker.fish
@@ -6,9 +6,11 @@ function uv-docker
         -e TERM \
         -e COLORTERM \
         -e TERM_PROGRAM \
+        -e UV_LINK_MODE=copy \
         -w "$working_dir" \
         -v "$working_dir:$working_dir" \
         -v "$HOME/.config/uv:/home/$username/.config/uv" \
+        -v "$HOME/.cache/uv:/home/$username/.cache/uv" \
         coding-agent-image:local \
         uv $argv
 end
